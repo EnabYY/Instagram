192cf8b4c54ce8e52a8d945931a4f1207fe95d14
EnabYY <49252487+EnabYY@users.noreply.github.com>
2019-08-31 15:49:19 -0400

    Add files via upload

---
+++ Instagram/recent.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Instagram</title>
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="./css/instastyle.css">
+	<link rel="stylesheet" type="text/css" href="./css/magnific.css">
+</head>
+<body>
+
+<div class="container">
+
+  <div id="instafeed" class="row"></div>
+
+
+<div class="overlay-image"><a href="LINK_URL">
+ <img class="image" src="IMAGE" alt="Alt text" />
+ <div class="normal">
+  <div class="text">Image + text
+   ORIGINAL</div>
+ </div>
+ <div class="hover">
+  <div class="text">Background + text
+   HOVER</div>
+ </div>
+</a></div>
+
+  </div>
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script type="text/javascript" src="./js/instafeed.min.js"></script>
+<script type="text/javascript" src="./js/custom.js"></script>
+<script type="text/javascript" src="./js/magnific.min.js"></script>
+
+</body>
+</html>
